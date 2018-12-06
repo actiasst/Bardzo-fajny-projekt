7d7a337901ad40e0755cbc0890dbf0747159a687
actiasst <maciekactias@gmail.com>
2018-12-06 20:33:20 +0100

    Add files via upload

--- testpdf.py
+++ testpdf.py
@@ -1,17 +1,27 @@
 sys.path.append("C:\\temp\\skrypty")
 
 from reportlab.pdfgen import canvas
-from reportlab.lib.pagesizes import letter
-from reportlab.lib.pagesizes import landscape
+from reportlab.lib.pagesizes import A4
+from reportlab.lib.pagesizes import portrait
 from reportlab.platypus import Image
 
-
-def generatePDF():
-	c = canvas.Canvas("TestPDF.pdf",pagesize=landscape(letter))
-
-	c.setFont('Helvetica',48, leading=None)
-	c.drawCentredString(415,500,"Bardzo ladny string")
+def generatePDF(list):
+	pageNumber = 1
+	beggining = 50
+	fontSize = 14
+	newLine = fontSize + 4
+	c = canvas.Canvas("skrypty\\TestPDF.pdf",pagesize=portrait(A4),bottomup=0)
+	c.setFont('Helvetica',fontSize, leading=None)
+	for tmp in range(len(list)):
+		if beggining >= 765:
+			c.drawCentredString(300,800,"- " + str(pageNumber) + " -")
+			pageNumber += 1
+			c.showPage()
+			beggining = 50
+			c.setFont('Helvetica',fontSize, leading=None)
+		c.drawString(50,beggining,list[tmp])
+		beggining += newLine
+	c.drawCentredString(300,800,"- " + str(pageNumber) + " -")
 	c.showPage()
 	c.save()
-generatePDF()
 print "done"
